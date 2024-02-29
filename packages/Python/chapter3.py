6108602fd1646427ad4a448e1ac98644d21894b2
Jaiber John <jaiber@remote-x.ai>
2024-02-29 18:56:23 +0530

    Chapter 3 and 4 completed!

--- packages/Python/chapter3.py
+++ packages/Python/chapter3.py
@@ -28,3 +28,66 @@ vec = VecToso3(exp_coord_rot)
 # Q9
 mexp = MatrixExp3(vec)
 print("mexp: ", mexp)
+
+
+T_SA = np.array([[0, -1, 0, 0], [0, 0, -1, 0], [1, 0, 0, 1], [0, 0, 0, 1]])
+T_SB = np.array([[1, 0, 0, 0], [0, 0, 1, 2], [0, -1, 0, 0], [0, 0, 0, 1]])
+
+T_AS = TransInv(T_SA)
+
+T_AB = np.dot(T_AS, T_SB)
+print("TAB: ", T_AB)
+
+p_b = np.array([1, 2, 3])
+
+# Q5
+p_s = np.dot(T_SB, np.append(p_b, 1))
+print("p_s: ", p_s)
+
+adj_T_AS = Adjoint(T_AS)
+print("adj_T_AS: ", adj_T_AS)
+V_s = np.array([3, 2, 1, -1, -2, -3])
+
+# Q6
+V_a = np.dot(adj_T_AS, V_s)
+print("V_a: ", V_a)
+
+# Q8
+# Calculate matrix logarithm of T_SA
+logT_SA = MatrixLog6(T_SA)
+print("logT_SA: ", logT_SA)
+# Get the rotation angle and axis from the matrix logarithm
+theta = so3ToVec(logT_SA[0:3, 0:3])
+# print theta in radians
+print("theta in radians: ", np.linalg.norm(theta))
+
+# Q9
+
+S_theta = np.array([0, 1, 2, 3, 0, 0])
+# Calculate the matrix exponential of the twist
+mexp = MatrixExp6(VecTose3(S_theta))
+print("mexp: ", mexp)
+
+# Q10
+F_b = np.array([1, 0, 0, 2, 1, 0])
+adj_T_SB = Adjoint(T_SB)
+# Calculate the representation of wrench in Frame B
+F_a = np.dot(adj_T_SB, F_b)
+print("F_a: ", F_a)
+
+# Q11
+T = np.array([[0, -1, 0, 3], [1, 0, 0, 0], [0, 0, 1, 1], [0, 0, 0, 1]])
+# Print transInv
+print("TransInv: ", TransInv(T))
+
+# Q14
+S_theta = np.array(
+    [[0, -1.5708, 0, 2.3562], [1.5708, 0, 0, -2.3562], [0, 0, 0, 1], [0, 0, 0, 0]]
+)
+# Print MatrixExp6
+print("MatExp6", MatrixExp6(S_theta))
+
+# Q15
+T = np.array([[0, -1, 0, 3], [1, 0, 0, 0], [0, 0, 1, 1], [0, 0, 0, 1]])
+# Print MatrixLog6
+print("MatrixLog6: ", MatrixLog6(T))
---
+++ packages/Python/chapter4.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+
+import math
+from modern_robotics import *
+
+
+def cross_mul(w, q):
+    w_so3mat = VecToso3(w)
+    return np.dot(-w_so3mat, q)
+
+
+print("Question 2")
+res = cross_mul(np.array([0, 0, 1]), np.array([1, 0, 0]))
+print("result 1 : ", res)
+
+res = cross_mul(np.array([0, 1, 0]), np.array([1, 0, 0]))
+print("result 2 : ", res)
+
+res = cross_mul(np.array([0, 1, 0]), np.array([2.732, 0, -1]))
+print("result 3 : ", res)
+
+res = cross_mul(np.array([0, 1, 0]), np.array([3.732, 0, 0.732]))
+print("result 4 : ", res)
+
+# res = cross_mul(np.array([0,0,1]), np.array([3.732,0,2.732]))
+# print ("result 5 : ", res)
+
+res = cross_mul(np.array([0, 0, 1]), np.array([3.732, 0, 2.732]))
+print("result 6 : ", res)
+
+print("Question 3")
+res = cross_mul(np.array([0, 0, 1]), np.array([-2.732, 0, -2.732]))
+print("result 1 : ", res)
+
+res = cross_mul(np.array([0, 1, 0]), np.array([-2.732, 0, -2.732]))
+print("result 2 : ", res)
+
+res = cross_mul(np.array([0, 1, 0]), np.array([-1, 0, -3.732]))
+print("result 3 : ", res)
+
+res = cross_mul(np.array([0, 1, 0]), np.array([0, 0, -2]))
+print("result 4 : ", res)
+
+res = cross_mul(np.array([0, 0, 0]), np.array([0, 0, -1]))
+print("result 5 : ", res)
+
+res = cross_mul(np.array([0, 0, 1]), np.array([0, 0, 0]))
+print("result 6 : ", res)
+
+np.set_printoptions(formatter={"float": lambda x: "{0:0.4f}".format(x)})
+
+# np.set_printoptions(precision=3)
+
+sq_rt3 = math.sqrt(3)
+
+print("Q4")
+M = np.array([[1, 0, 0, 2 + sq_rt3], [0, 1, 0, 0], [0, 0, 1, 1 + sq_rt3], [0, 0, 0, 1]])
+
+Slist = np.array(
+    [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [1, 0, 0, 0, 0, 1],
+        [0, 0, 1, -(sq_rt3 - 1), 0, 0],
+        [-1, 0, 0, 0, 0, -(2 + sq_rt3)],
+        [0, 1, 1 + sq_rt3, 2 + sq_rt3, 1, 0],
+    ]
+)
+Blist = np.array(
+    [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [1, 0, 0, 0, 0, 1],
+        [0, 1 + sq_rt3, 2 + sq_rt3, 2, 0, 0],
+        [1 + sq_rt3, 0, 0, 0, 0, 0],
+        [0, -(1 + sq_rt3), -1, 0, 1, 0],
+    ]
+)
+thetalist = np.array(
+    [-math.pi / 2, math.pi / 2, math.pi / 3, -math.pi / 4, 1, math.pi / 6]
+)
+
+T = FKinSpace(M, Slist, thetalist)
+print("T array")
+print(T)
+
+print("Q5")
+T = FKinBody(M, Blist, thetalist)
+print("T array")
+print(T)
